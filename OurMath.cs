a54ae4f2344c2c36cd47f8da0f8fb5c9f9596473
Avi Weinberg <aweinber18@student.touro.edu>
2023-12-20 22:57:38 -0500

    started the tzurah

---
+++ OurMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class OurMath
+{
+	public static int[] MultiplyTwoRows(int[] a, int[] b)
+	{
+		return null;
+	}
+
+	public static int[] MultiplyColumns(int[] a, int[] b) 
+	{
+		return null; 
+	}
+
+    public static int[] SumOneRow(int[] a)
+	{
+		return null;
+	}
+
+	public static int CompareTo(int[][] a, int[][] b)
+	{
+		int acc = 0;
+		for (int i = 0; i < a.Length; i++)
+		{
+			for (int j = 0; j < a[i].Length; j++)
+			{
+				acc += Math.Max(a[i][j], b[i][j]) - Math.Min(a[i][j], b[i][j]);
+			}
+		}
+		return 0;
+	}
+
+
+}
