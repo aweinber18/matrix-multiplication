cef8b94b4718cc77052c2bf65547f26abc8b728e
Avi Weinberg <aweinber18@student.touro.edu>
2024-01-07 13:05:54 -0500

    added 3*5 5*7 1*3 3*1 effective naming

--- OurMath.cs
+++ OurMath.cs
@@ -2,7 +2,56 @@
 
 public class OurMath
 {
-	public static int[] MultiplyTwoRows(int[] a, int[] b)
+    public static double[,] MatrixMultiply(double[,] a, double[,] b)
+    {
+        int rowsA = a.GetLength(0);
+        int colsA = a.GetLength(1);
+        int colsB = b.GetLength(1);
+
+        double[,] result = new double[rowsA, colsB];
+
+        for (int i = 0; i < rowsA; i++)
+        {
+            for (int j = 0; j < colsB; j++)
+            {
+                double sum = 0;
+                for (int k = 0; k < colsA; k++)
+                {
+                    sum += a[i, k] * b[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+
+        return result;
+    }
+
+    public static void PrintMatrix(string title, double[,] matrix1, double[,] matrix2)
+    {
+        Console.WriteLine(title);
+        Console.WriteLine("AB:");
+        PrintMatrix(matrix1);
+        Console.WriteLine("BA:");
+        PrintMatrix(matrix2);
+        Console.WriteLine();
+    }
+
+    public static void PrintMatrix(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+public static int[] MultiplyTwoRows(int[] a, int[] b)
 	{
 		return null;
 	}
